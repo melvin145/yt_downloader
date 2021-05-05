2802789f721e87996528f03263b9d4820c502cdf
melvin145 <pythonnoobie4@gmail.com>
2021-05-05 16:35:20 +0530

    dk

--- home/views.py
+++ home/views.py
@@ -7,11 +7,12 @@ def home(request):
     return render(request,"home.html")
 
 def downloader(request):
+    SAVE_PATH="E:/"
     if request.method=='POST':
         url=request.POST['url']
         video=YouTube(url)
         tittle=YouTube(url).title
-        video.streams.get_highest_resolution().download()
+        video.streams.get_lowest_resolution().download(SAVE_PATH)
         return render(request,'succes.html',{'title':tittle}) 
     else:
         return redirect('home')   
