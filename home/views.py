05d9ac266a870e593d27cbad94b106447f319e89
melvin145 <pythonnoobie4@gmail.com>
2021-05-05 17:38:03 +0530

    'lasteve'

--- home/views.py
+++ home/views.py
@@ -9,7 +9,7 @@ def downloader(request):
     SAVE_PATH="E:/"
     if request.method=='POST':
         video_url=request.POST['url']
-        video=YouTube(video_url).streams.first().download(skip_existing=True)
+        video=YouTube(video_url).streams.get_highest_resolution().download(skip_existing=True)
         
         return FileResponse(open(video,'rb'),as_attachment=True)
     else:
