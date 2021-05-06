18054eaae0be53fb1b0fe2c35f3e151f7a2580f1
melvin145 <pythonnoobie4@gmail.com>
2021-05-06 18:26:22 +0530

    'added_audio_feature

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from django.contrib import admin
-from .views import home,downloader
+from .views import home,downloader,audio
 urlpatterns=[
     path("" ,home,name="home"),
     path("download",downloader,name="downloader"),
+    path("audio",audio,name='audio_downloader')
     
 ]
 
--- home/views.py
+++ home/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render,redirect
 #from pytube import YouTube
 from django.http import FileResponse
 from pytube import YouTube
+import pafy
 def home(request):
     return render(request,"home.html")
 
 def downloader(request):
-    SAVE_PATH="E:/"
+    #SAVE_PATH="E:/"
     if request.method=='POST':
         video_url=request.POST['url']
         video=YouTube(video_url).streams.get_highest_resolution().download(skip_existing=True)
@@ -16,5 +17,18 @@ def downloader(request):
         return redirect('home')   
     return redirect('home')   
     
+def audio(request):
+    if request.method=='POST':
+        url=request.POST['url']
+        audio_url=YouTube(url)
+        audio_to_download=audio_url.streams.filter(only_audio=True).all()
+        audio=audio_to_download[1].download()
+
+       
+        return FileResponse(open(audio,'rb'),as_attachment=True,)
+        
+
+
+    return render(request,'audio_downloader.html')
 
 
